14e4332720aeab59ffc851ff655da46647025531
Harm Fennis <harmfennis@kassy.nl>
2025-03-10 14:01:10 +0100

    Enhance header navigation with active link highlighting and update CSS styles for improved visual consistency

--- sections/header.php
+++ sections/header.php
@@ -1,13 +1,16 @@
+<?php
+$currentPage = basename(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), ".php");
+$active = 'class="active"';
+?>
 <header>
-    <!-- <h1>Vakantie Villa</h1> -->
     <a href="/">
         <img src="/assets/img/logo.svg" alt="Vakantie Villa">
     </a>
     <nav>
         <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="villa.php">Villas</a></li>
-            <li><a href="contact.php">Contact</a></li>
+            <li><a <?php if ($currentPage == 'index' || $currentPage == '') echo $active; ?> href="index.php">Home</a></li>
+            <li><a <?php if ($currentPage == 'villa') echo $active; ?> href="villa.php">Villas</a></li>
+            <li><a <?php if ($currentPage == 'contact') echo $active; ?> href="contact.php">Contact</a></li>
         </ul>
     </nav>
 </header>
