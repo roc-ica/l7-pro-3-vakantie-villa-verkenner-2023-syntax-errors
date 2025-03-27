82c1ac2fb8e4245c93540e0dfb940b59b8e4ef66
Xpleayer <Rouzbeh.sayadifar@gmail.com>
2025-03-27 14:31:45 +0100

    Changed the tailwind css to normal css. tailwind sucks a**

--- sections/texblock_media.php
+++ sections/texblock_media.php
@@ -5,12 +5,12 @@
                 <h1>Textblock-media</h1>
             </div>
             <div class="content-wrapper">
-                <p class="mb-3 text-gray-500 dark:text-gray-400">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
-                <p class="text-gray-500 dark:text-gray-400">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease, with a complete audit trail for every change.</p>
+                <p>Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
+                <p>Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease, with a complete audit trail for every change.</p>
             </div>
             <div class="button-wrapper">
                 <a href="../index.php">
-                    <button type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Alternative</button>
+                    <button type="button" class="button --primary">Alternative</button>
                 </a>
             </div>
         </div>
