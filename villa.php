9c5e1357eed218c66880f7dc2c0196b4e2a9c4f9
Mark Datema <147700456+Auguste111@users.noreply.github.com>
2025-04-04 09:13:27 +0100

    basics for filter page

---
+++ villa.php
@@ -0,0 +1,12 @@
+<?php include 'includes/data.php'; ?>
+<!DOCTYPE html>
+<html lang="nl">
+<?php include 'includes/head.php'; ?>
+<body>
+    <?php include 'sections/navigation.php'; ?>
+    <main id="site-content">
+        
+    </main>
+    <?php include 'sections/footer.php'; ?>
+</body>
+</html>
