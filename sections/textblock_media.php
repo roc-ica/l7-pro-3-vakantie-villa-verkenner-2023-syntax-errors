d6438f0dc0a3c0f5f62bbb6eaa012bc8699020e0
Xpleayer <Rouzbeh.sayadifar@gmail.com>
2025-04-07 15:08:30 +0200

    Navigation styling geupdate

--- sections/textblock_media.php
+++ sections/textblock_media.php
@@ -1,24 +1,26 @@
 <section class="textblock-media">
     <div class="textblock-media__container">
         <div class="textblock-media__content --left">
-            <div class="heading">
+            <div class="textblock-media__heading">
                 <h1>Textblock-media</h1>
             </div>
-            <div class="content-wrapper">
+            <div class="textblock-media__content">
                 <p>Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
                 <p>Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease, with a complete audit trail for every change.</p>
             </div>
-            <div class="button-wrapper">
+            <div class="buttons">
+                <p>
+                    See for yourself!
+                </p>
                 <a href="../index.php">
                     <button type="button" class="button --primary">Alternative</button>
                 </a>
             </div>
         </div>
         <div class="textblock-media__media --right --image">
-            <picture class="media_image">
+            <picture class="textblock-media__image">
                 <img src="../assets/img/villa/examplevilla2-feyg827.jpg" alt="alternative">
             </picture>
         </div>
-
     </div>
 </section>
